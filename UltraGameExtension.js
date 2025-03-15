8fabaeed7e39d3006b00b9ced08363bb98081e19
natounet21 <149017981+Nathan-DG@users.noreply.github.com>
2025-03-15 02:05:47 -0400

    Update UltraGameExtension.js

--- UltraGameExtension.js
+++ UltraGameExtension.js
@@ -1,134 +1,114 @@
+// Name: Comment Blocks
+// ID: lmscomments
+// Description: Annotate your scripts.
+// By: LilyMakesThings <https://scratch.mit.edu/users/LilyMakesThings/>
+// License: MIT AND LGPL-3.0
 
-    var ext = {}
-    const ArgumentType = Scratch.ArgumentType;
-    const BlockType = Scratch.BlockType;
+///* generated l10n code */Scratch.translate.setup({"de":{"_Comment Blocks":"Kommentar Blöcke"},"fi":{"_Comment Blocks":"Kommenttilohkot","_comment":"kommentti"},"it":{"_Comment Blocks":"Blocchi per Commenti","_comment":"commento"},"ja":{"_Comment Blocks":"コメントブロック","_comment":"コメント"},"ko":{"_Comment Blocks":"주석 블록","_comment":"주석"},"nb":{"_Comment Blocks":"Kommentarblokker","_comment":"kommentar"},"nl":{"_Comment Blocks":"Commentaarblokken","_comment":"opmerking"},"ru":{"_Comment Blocks":"Блоки Комментариев","_comment":"комментарий"},"tr":{"_Comment Blocks":"Yorum Blokları"},"uk":{"_Comment Blocks":"Блоки-Коментарі"},"zh-cn":{"_Comment Blocks":"注释","_comment":"注释"}});/* end generated l10n code */(function (Scratch) {
+    "use strict";
 
-    // Define the extension metadata
-    window.getInfo = function() {
-        return {
-            id: 'ultragame',
-            name: 'Ultra Game',
-            color1: '#000000',
-            color2: '#000000',
-            blockIconURI: 'https://ultra-game.fun/favicon.ico',
-            menuIconURI: 'https://ultra-game.fun/favicon.ico',
-            blocks: [
-                {
-                    opcode: 'ug_userexists',
-                    blockType: BlockType.BOOLEAN,
-                    text: 'User exists [USERNAME]',
-                    arguments: {
-                        USERNAME: {
-                            type: ArgumentType.STRING,
-                            defaultValue: 'UltraGame'
-                        }
-                    }
-                },
-                {
-                    opcode: 'ug_getbio',
-                    blockType: BlockType.REPORTER,
-                    text: 'Get bio [USERNAME]',
-                    arguments: {
-                        USERNAME: {
-                            type: ArgumentType.STRING,
-                            defaultValue: 'UltraGame'
-                        }
-                    }
-                },
-                {
-                    opcode: 'ug_getbadges',
-                    blockType: BlockType.REPORTER,
-                    text: 'Get badges [USERNAME]',
-                    arguments: {
-                        USERNAME: {
-                            type: ArgumentType.STRING,
-                            defaultValue: 'UltraGame'
-                        }
-                    }
-                },
-                {
-                    opcode: 'ug_signup',
-                    blockType: BlockType.COMMAND,
-                    text: 'Create account! Username: [USERNAME] Password: [PASSWORD]',
-                    arguments: {
-                        USERNAME: {
-                            type: ArgumentType.STRING,
-                            defaultValue: ''
-                        },
-                        PASSWORD: {
-                            type: ArgumentType.STRING,
-                            defaultValue: ''
-                        }
-                    }
-                },
-                {
-                    opcode: 'ug_chat',
-                    blockType: BlockType.COMMAND,
-                    text: 'Chat in the chat! Username: [USERNAME] Password: [PASSWORD] Content: [CONTENT]',
-                    arguments: {
-                        USERNAME: {
-                            type: ArgumentType.STRING,
-                            defaultValue: ''
-                        },
-                        PASSWORD: {
-                            type: ArgumentType.STRING,
-                            defaultValue: ''
-                        },
-                        CONTENT: {
-                            type: ArgumentType.STRING,
-                            defaultValue: ''
-                        }
-                    }
-                }
-            ]
-        };
-    };
-
-    // Block implementations
-
-    window.ug_userexists = function(args, util) {
-        return fetch("https://ultra-game.fun/api/find?username=" + args.USERNAME)
-            .then(response => response.json())
-            .then(data => data.response);
-    };
-
-    window.ug_getbio = function(args, util) {
-        return fetch("https://ultra-game.fun/api/getbio?username=" + args.USERNAME)
-            .then(response => response.json())
-            .then(data => data.response ? data.data : "404 Not found");
-    };
-
-    window.ug_getbadges = function(args, util) {
-        return fetch("https://ultra-game.fun/api/find?username=" + args.USERNAME)
-            .then(response => response.json())
-            .then(data => {
-                if (data.response) {
-                    return fetch("https://ultra-game.fun/api/getbadges?username=" + args.USERNAME)
-                        .then(response => response.json())
-                        .then(badges => badges.join(" "));
-                } else {
-                    return "404 Not found";
-                }
-            });
-    };
-
-    window.ug_signup = function(args, util) {
-        return fetch("https://ultra-game.fun/api/signup?username=" + args.USERNAME + "&password=" + args.PASSWORD)
-            .then(response => response.json())
-            .catch(err => {
-                console.error('Signup failed:', err);
-            });
-    };
-
-    window.ug_chat = function(args, util) {
-        const socket = io('https://ultra-game.fun');
-        socket.emit('message', {
-            content: args.CONTENT,
-            username: args.USERNAME,
-            password: args.PASSWORD
+    class CommentBlocks {
+      getInfo() {
+        const defaultValue = Scratch.translate({
+          default: "comment",
+          description: "Default comment value",
         });
-    };
-
-    // Register the extension
-    Scratch.extensions.register(ext);
-
+        return {
+          id: "lmscomments",
+          name: Scratch.translate("Comment Blocks"),
+          color1: "#e4db8c",
+          color2: "#c6be79",
+          color3: "#a8a167",
+          blocks: [
+            /* eslint-disable extension/should-translate */
+            {
+              opcode: "commentHat",
+              blockType: Scratch.BlockType.HAT,
+              text: "// [COMMENT]",
+              isEdgeActivated: false,
+              arguments: {
+                COMMENT: {
+                  type: Scratch.ArgumentType.STRING,
+                  defaultValue: defaultValue,
+                },
+              },
+            },
+            {
+              opcode: "commentCommand",
+              blockType: Scratch.BlockType.COMMAND,
+              text: "// [COMMENT]",
+              arguments: {
+                COMMENT: {
+                  type: Scratch.ArgumentType.STRING,
+                  defaultValue: defaultValue,
+                },
+              },
+            },
+            {
+              opcode: "commentC",
+              blockType: Scratch.BlockType.CONDITIONAL,
+              text: "// [COMMENT]",
+              arguments: {
+                COMMENT: {
+                  type: Scratch.ArgumentType.STRING,
+                  defaultValue: defaultValue,
+                },
+              },
+            },
+            {
+              opcode: "commentReporter",
+              blockType: Scratch.BlockType.REPORTER,
+              text: "[INPUT] // [COMMENT]",
+              allowDropAnywhere: true,
+              arguments: {
+                COMMENT: {
+                  type: Scratch.ArgumentType.STRING,
+                  defaultValue: defaultValue,
+                },
+                INPUT: {
+                  type: Scratch.ArgumentType.STRING,
+                  defaultValue: "",
+                },
+              },
+            },
+            {
+              opcode: "commentBoolean",
+              blockType: Scratch.BlockType.BOOLEAN,
+              text: "[INPUT] // [COMMENT]",
+              arguments: {
+                COMMENT: {
+                  type: Scratch.ArgumentType.STRING,
+                  defaultValue: defaultValue,
+                },
+                INPUT: {
+                  type: Scratch.ArgumentType.BOOLEAN,
+                },
+              },
+            },
+            /* eslint-enable extension/should-translate */
+          ],
+        };
+      }
+  
+      commentHat() {
+        // no-op
+      }
+  
+      commentCommand() {
+        // no-op
+      }
+  
+      commentC(args, util) {
+        return true;
+      }
+  
+      commentReporter(args) {
+        return args.INPUT;
+      }
+  
+      commentBoolean(args) {
+        return args.INPUT || false;
+      }
+    }
+    Scratch.extensions.register(new CommentBlocks());
+  })(Scratch);
